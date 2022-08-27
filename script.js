bb6224933ecacc6739297e4441fa1871af7f54ef
kyubhinhan <108394338+kyubhinhan@users.noreply.github.com>
2022-08-27 17:32:44 +0900

    add opinion page change db
    Opinion page를 만들었고, 저장하는 db 형식을 변경하였다.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
   const $AddSubjectPage = get('.addsubject')
   const $subjecttextareaInput = get('.SubjectTextareaInput')
   const $subjectDeleteBtns = getAll('.SubjectDeleteBtn')
+  const $OpinionPage = get('.OpinionPage')
 
 
   let currentPage = 1
@@ -79,12 +80,12 @@
     SubjectElement.classList.add('subjectdisplay')
     SubjectElement.dataset.key = id
     SubjectElement.innerHTML = `
-    <div class="subject">
+    <div class="subject" data-key="${id}">
     ${contents}
     </div>
     <div class="OpinionCount">
       <i class="fa-solid fa-shoe-prints"></i>
-      : ${comments}
+      : ${comments.length}
     </div>
     <button class="SubjectDeleteBtn">
       <i class="fa-solid fa-xmark"></i>
@@ -116,7 +117,7 @@
     }
     const subjects = {
       "contents": InputSubjectContents,
-      "comments": 0
+      "comments": []
     }
     fetch(`${APIURL}subjects`, {
       method: 'POST',
@@ -155,7 +156,9 @@
     })
     $SubmitSubjectBtn.addEventListener('click', addSubjectToServer)
   }
-
+  /**
+   * 주제 입력창을 가리는 함수
+   */
   const hideSubjectInput = () => {
     $AddSubjectPage.classList.toggle('hidden', true)
     $subjecttextareaInput.value = ''
@@ -178,6 +181,80 @@
         .catch((error) => console.error(error.message))
     }
   }
+
+  //key를 id로 가진 subject만 서버에서 받아오는 함수
+  const getSubject = async(key)=>{
+    //async는 무조건 promise를 반환한다..
+    try{
+      let response = await (await fetch(`${APIURL}subjects/${key}`)).json()
+      return response
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  //Opinion page element를 만들어주는 함수
+  const createOpinionpageelement = (subject)=>{
+    let {id, contents, commments} = subject
+    let $opinionpageelement = document.createElement('div')
+    $opinionpageelement.classList.add('OpinionDisplay')
+    $opinionpageelement.innerHTML=`
+    <div class="opinionInput">
+      <div class="OpinionSubject">${contents}</div>
+        <form action="" class="form_input">
+          <textarea
+            name="content"
+            id="content"
+            cols="50"
+            rows="6"
+            placeholder="주제와 관련된 의견을 들려주세요"
+            class="TextareaInput"
+        ></textarea>
+        <input type="submit" class="submit_btn" />
+        </form>
+      </div>
+      <div class="lastOpinion">
+        <h2 class="lastOpinionTitle">지난 의견</h2>
+        <div class="OpinionCase">
+          <div class="OpinionDate">22년 8월 22일</div>
+          <div class="OpinionContent">지난 의견 2</div>
+        </div>
+        <div class="OpinionCase">
+          <div class="OpinionDate">22년 8월 20일</div>
+          <div class="OpinionContent">지난 의견 1</div>
+        </div>
+      </div>
+    </div>
+    `
+
+    return $opinionpageelement
+  }
+
+  //Opinion page를 보여주어 조작 가능하게 만드는 함수
+  const showOpinionpage = (subject)=>{
+    $OpinionPage.innerHTML = ''
+    $OpinionPage.classList.toggle('hidden',false)
+    $OpinionPage.appendChild(createOpinionpageelement(subject))
+    
+  }
+
+  const addcomment = async(e)=>{
+    let targetClasses = Array.from(e.target.classList)
+    if(targetClasses.includes('subject') || targetClasses.includes('subjectdisplay')){
+      //잡히면, data set key를 받아서 서버에서 데이터를 id로 받아와서
+      //Comment element를 만든 다음에
+      //Opinion Page에 붙여주면 되겠는데.. 이때 Opinion page를 먼저 초기화해주고
+
+      //Subject display의 데이터 id 받아오기
+      let key = e.target.dataset.key
+      //관련된 id의 subject를 받아오기(getSubject 함수 사용)
+      let subject = await getSubject(key)
+      //Opinionpage 보여주기
+      showOpinionpage(subject)
+
+    }
+  }
+
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
       getSubjects()
@@ -189,24 +266,9 @@
     //주제 삭제해주기
     $subjectlist.addEventListener('click', deletesubject)
 
-    window.addEventListener('keydown', (e) => {
-      if (e.key === 'Escape') {
-        e.preventDefault()
-        e.stopPropagation()
-        fetch(`${APIURL}subjects`, {
-          method: 'POST',
-          headers: { 'Content-type': 'application/json' },
-          body: JSON.stringify({
-            "contents": '테스트요',
-            "comments": 0
-          }),
-        })
-          .then((response) => response.json())
-          .then(getSubjects)
-          .then(console.log('wow'))
-          .catch(error => console.log(error))
-      }
-    })
+    //주제 선택했을 때, 관련된 의견을 넣을 수 있도록 만들기
+    $subjectlist.addEventListener('click',addcomment)
+
   }
 
   init()
